c068eb7458586e4e0af420766e6e0adaad00359f
vinovad <advaskan@gmail.com>
2026-02-19 17:10:15 +0300

    Update review.py

--- PPOIS/lab1/domain/review.py
+++ PPOIS/lab1/domain/review.py
@@ -1,16 +1,24 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .product import Product
+
 
 class Review:
     def __init__(self, product_id: str, author: str, comment: str, rating: int) -> None:
         if not (1 <= rating <= 5):
             raise ValueError("Рейтинг должен быть от 1 до 5")
         
-        self.product_id = product_id
-        self.author = author
-        self.comment = comment
-        self.rating = rating
-        self.date = datetime.now()
+        self.product_id: str = product_id
+        self.author: str = author
+        self.comment: str = comment
+        self.rating: int = rating
+        self.date: datetime = datetime.now()
     
     def __str__(self) -> str:
-        return (f"Review(ProductID={self.product_id}, Author={self.author}, "
-                f"Rating={self.rating}/5, Date={self.date.strftime('%Y-%m-%d')})")
+        """Строковое представление отзыва"""
+        return (
+            f"Review(ProductID={self.product_id}, Author={self.author}, "
+            f"Rating={self.rating}/5, Date={self.date.strftime('%Y-%m-%d')})"
+        )
