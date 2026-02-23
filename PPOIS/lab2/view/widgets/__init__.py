0449761801441418e56b282cb2298698d95becde
vinovad <advaskan@gmail.com>
2026-02-23 16:58:05 +0300

    Create __init__.py

---
+++ PPOIS/lab2/view/widgets/__init__.py
@@ -0,0 +1,10 @@
+"""
+__init__.py - инициализация пакета widgets
+
+Этот файл позволяет импортировать виджеты напрямую из пакета widgets:
+from view.widgets import Pagination
+"""
+
+from .pagination import Pagination
+
+__all__ = ['Pagination']
